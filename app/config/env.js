ce054c6298e6db194559dca8d85ee8c9337383e5
Checha <99372151+checha-afg@users.noreply.github.com>
2024-09-07 22:22:36 -0600

    connect success
    Co-Authored-By: Josué Oliva <78836905+Joliva07@users.noreply.github.com>

--- app/config/env.js
+++ app/config/env.js
@@ -15,9 +15,9 @@
   */
  // env.js
  module.exports = {
-  DB_USER: 'C##DB_FAZBEAR_USR',
+  DB_USER: 'C##DB_FAZBEAR_CHECHA',
   DB_PASSWORD: 'password',
   DB_HOST: '127.0.0.1',  // Cambia Pandnaceous por 127.0.0.1 para asegurar la conexión local
-  DB_PORT: '1522',
+  DB_PORT: '1521',
   DB_NAME: 'xe'  // Cambia FAZBEAR_DB por xe, que es el servicio que está registrado
 };
